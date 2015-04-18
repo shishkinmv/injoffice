20933d45c10b062b84a1367dc309d31b4cb16f18
shishkinmv <shishkinmv89@gmail.com>
2015-04-18 12:15:45 +0300

    Update buildclim.py

--- climatology/buildclim.py
+++ climatology/buildclim.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+class BuildClim():
+  def __init__(self,place):
+    self.data=self.get_clima_data(place)
+  def get_clima_data(self,place):
+    data={}
+    return data
